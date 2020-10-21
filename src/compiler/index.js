992adc8fd28ae3f386defc99fcc99fe99aedbc36
xiaowangdong <62345592+xiaowangdong@users.noreply.github.com>
2020-10-21 17:26:57 +0800

    10-21

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -9,17 +9,22 @@ import { createCompilerCreator } from './create-compiler'
 // parser/optimizer/codegen, e.g the SSR optimizing compiler.
 // Here we just export a default compiler using the default parts.
 export const createCompiler = createCompilerCreator(function baseCompile (
-  template: string,
-  options: CompilerOptions
+	template: string,
+	options: CompilerOptions
 ): CompiledResult {
-  const ast = parse(template.trim(), options)
-  if (options.optimize !== false) {
-    optimize(ast, options)
-  }
-  const code = generate(ast, options)
-  return {
-    ast,
-    render: code.render,
-    staticRenderFns: code.staticRenderFns
-  }
+	// 1.解析：模板转换为对象（称为AST，抽象语法树）
+	const ast = parse(template.trim(), options)
+
+	// 2.优化操作：标记静态节点，diff时可以直接跳过
+	if (options.optimize !== false) {
+		optimize(ast, options)
+	}
+
+	// 3.代码生成：转换AST为代码字符串  new Function(code)就可以得到真正的渲染函数
+	const code = generate(ast, options)
+	return {
+		ast,
+		render: code.render,
+		staticRenderFns: code.staticRenderFns
+	}
 })
